6b05e154392368fa3a2427683cac684f3e1f64fc
Droyd97 <55035076+Droyd97@users.noreply.github.com>
2023-10-08 21:05:16 +0100

    Finished lookup for fx manager

--- src/data/fx_manager.rs
+++ src/data/fx_manager.rs
@@ -5,7 +5,7 @@ use crate::security::Currency;
 
 pub struct FXManager<T> where T: DataNumberType {
 
-    data: HashMap<(Currency, Currency), T>
+    data: HashMap<(Currency, Currency), ExchangeRate<T>>
 
 }
 
@@ -19,13 +19,52 @@ impl<T> FXManager<T> where T: DataNumberType {
     }
 
     pub fn update(&mut self, source: Currency, target: Currency, rate: T) {
-        self.data.insert((source, target), rate);
+        self.data.insert((source, target), ExchangeRate::new(source, target, rate));
     }
 
+    pub fn get_rate(&self, source: Currency, target: Currency) -> Option<ExchangeRate<T>> {
+        self.data.get(&(source, target)).map(|x| x.clone())
+    }
 
+    pub fn contains(&self, source: Currency, target: Currency) -> bool {
+        self.data.contains_key(&(source, target))
+    }
+
+    pub fn lookup(&self, source: Currency, target: Currency) -> Option<ExchangeRate<T>> {
+        if source == target {
+            return Some(ExchangeRate::new(source, target, <i8 as Into<T>>::into(1)))
+        }
+
+        if self.contains(source, target) {
+            return self.get_rate(source, target)
+        }
+
+        for ((tmp_source, tmp_target), rate) in &self.data {
+
+
+            let other = if source == rate.source {
+                rate.target
+            } else {
+                rate.source
+            };
+
+            let (s, t) = if (tmp_source == &source) {
+                (source, target)
+            } else {
+                return None
+            };
+
+            if let Some(head) = self.get_rate(s, other) {
+                if let Some(tail) = self.lookup(other, t) {
+                    return ExchangeRate::chain(&head, &tail).ok()
+                }
+            }
+        }
+        None
+    }
 }
 
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug, Clone, Eq, PartialEq)]
 pub struct ExchangeRate<T> where T: DataNumberType {
     source: Currency,
     target: Currency,
@@ -90,6 +129,94 @@ impl<T> ExchangeRate<T> where T: DataNumberType {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+
+    #[test]
+    fn test_fx_manager_contains_true() {
+        // Arrange
+
+        let mut manager = FXManager::new();
+
+        manager.data.insert(
+            (Currency::USD, Currency::JPY),
+            ExchangeRate::new(
+                Currency::USD,
+                Currency::JPY,
+                8.0
+            )
+        );
+
+        let expected = true;
+
+        // Act
+        let result = manager.contains(Currency::USD, Currency::JPY);
+
+        // Assert
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn test_fx_manager_contains_false() {
+        // Arrange
+
+        let mut manager = FXManager::new();
+
+        manager.data.insert(
+            (Currency::USD, Currency::JPY),
+            ExchangeRate::new(
+                Currency::USD,
+                Currency::JPY,
+                8.0
+            )
+        );
+
+        let expected = false;
+
+        // Act
+        let result = manager.contains(Currency::JPY, Currency::GBP);
+
+        // Assert
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn test_fx_manager_lookup_triangulation() {
+        // Arrange
+        let mut manager = FXManager::new();
+
+        manager.data.insert(
+            (Currency::USD, Currency::JPY),
+            ExchangeRate::new(
+                Currency::USD,
+                Currency::JPY,
+                8.0
+            )
+        );
+
+        manager.data.insert(
+            (Currency::JPY, Currency::GBP),
+            ExchangeRate::new(
+                Currency::JPY,
+                Currency::GBP,
+                4.0
+            )
+        );
+
+        let expected = ExchangeRate::new(
+            Currency::USD,
+            Currency::GBP,
+            32.0
+        );
+
+        // Act
+        let result = manager
+            .lookup(Currency::USD, Currency::GBP)
+            .unwrap();
+
+
+        // Assert
+        assert_eq!(result, expected)
+    }
 
     #[test]
     fn test_exchange_rate_chain_same_sources() {
